81e1577a47319c78dadad1ff0a749e1f52e79504
usaravanan <usaravananbe@gmail.com>
2022-03-04 17:20:01 +0530

    Add files via upload

---
+++ junit/JunitRunner.java
@@ -0,0 +1,8 @@
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("com.app.demo.suite1")
+public class JunitRunner {
+}
