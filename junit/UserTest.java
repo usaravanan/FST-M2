78cd362dfda80e53675b328a65af4e2efe289b5c
usaravanan <usaravananbe@gmail.com>
2022-03-04 17:19:22 +0530

    Update UserTest.java

--- junit/UserTest.java
+++ junit/UserTest.java
@@ -1,23 +1,14 @@
 package com.app.demo.suite1;
 
-import com.add.demo.UserRepository;
-import com.add.demo.UserService;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static java.time.Duration.ofSeconds;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 class UserTest {
 
@@ -48,18 +39,18 @@ class UserTest {
     @DisplayName("getUserByIdForGivenInputIsOne")
     @Order(1)
     void getUsers() {
-       assertAll(()-> {
-           assertEquals("user1", names.get(0));
-           assertEquals("user2", names.get(1));
-       });
+        assertAll(() -> {
+            assertEquals("user1", names.get(0));
+            assertEquals("user2", names.get(1));
+        });
     }
 
 
     @ParameterizedTest
-    @ValueSource(ints = {0,1})
-    //@CsvSource({"apple,1","2","3"}) set the param accordingly
+    @ValueSource(ints = {0, 1})
+        //@CsvSource({"apple,1","2","3"}) set the param accordingly
     void getUserByIdForGivenInputFromDataSet(int index) {
-        assertTimeout(ofSeconds(10), ()-> assertEquals(true, names.get(index).contains("user")));
+        assertTimeout(ofSeconds(10), () -> assertEquals(true, names.get(index).contains("user")));
     }
 
     @Test
